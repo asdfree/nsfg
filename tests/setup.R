cd6ee0b75dcd3df367ec6da170da92719d047a05
Anthony Damico <ajdamico@gmail.com>
2024-03-19 18:57:37 -0400

    vacs

--- tests/setup.R
+++ tests/setup.R
@@ -34,9 +34,9 @@ nsfg_df <- data.frame( nsfg_tbl )
 # nsfg_fn <- file.path( path.expand( "~" ) , "NSFG" , "this_file.rds" )
 # saveRDS( nsfg_df , file = nsfg_fn , compress = FALSE )
 # nsfg_df <- readRDS( nsfg_fn )
-options( survey.lonely.psu = "adjust" )
-
 library(survey)
+
+options( survey.lonely.psu = "adjust" )
 
 nsfg_design <- 
 	svydesign( 
